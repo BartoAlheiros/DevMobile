ea6d01a86a696fdc4b75b010ceaebf2c08a0636e
Bartô Alheiros <alheirosb@gmail.com>
2018-10-31 14:54:17 -0300

    AppTwitter2

---
+++ AppTwitterBartolomeu2/AppTwitterBartolomeu2/app/src/main/java/ufrpe/br/apptwitterbartolomeu2/MainActivity.kt
@@ -0,0 +1,12 @@
+package ufrpe.br.apptwitterbartolomeu2
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
